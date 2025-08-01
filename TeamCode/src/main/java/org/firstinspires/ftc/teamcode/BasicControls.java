69a95ac53ffa8e0b4453213eda843aa17fce4dfb
NexelusBleu <nexelusbleu3@gmail.com>
2025-08-01 15:48:42 -0400

    Add turn control and power normalization to drive logic
    Introduces turn input from the right stick and updates motor power calculations to include turning. Adds normalization to motor powers when values exceed allowed range to prevent errors and ensure consistent robot movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicControls.java
@@ -25,17 +25,33 @@ public class BasicControls extends OpMode {
     public void loop() {
         double pad1LY = -gamepad1.left_stick_y;
         double pad1LX = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
 
         double r = Math.sqrt(Math.pow(pad1LX,2) + Math.pow(pad1LY,2));
         double angle = Math.atan2(pad1LY,pad1LX);
         //Turning inputs into polar form
 
-        double BLFR = Math.sin(angle - (double) 1 /4*pi)*r;
-        double  BRFL = Math.sin(angle + (double) 1 /4*pi)*r;
-        frontLeft.setPower(BRFL);
-        backRight.setPower(BRFL);
-        frontRight.setPower(BLFR);
-        backLeft.setPower(BLFR);
+        double BLFR = (Math.sin(angle - (double) 1 /4*pi)*r) + turn;
+        double  BRFL = (Math.sin(angle + (double) 1 /4*pi)*r) + turn;
+        if(BLFR<-1 || BLFR>1 || BRFL<-1 || BRFL>1) {
+            if(BLFR<-1 || BLFR>1){
+                frontLeft.setPower(-BRFL/Math.abs(BLFR));
+                backRight.setPower(BRFL/Math.abs(BLFR));
+                frontRight.setPower(BLFR/Math.abs(BLFR));
+                backLeft.setPower(-BLFR/Math.abs(BLFR));
+            } else {
+                frontLeft.setPower(-BRFL/Math.abs(BRFL));
+                backRight.setPower(BRFL/Math.abs(BRFL));
+                frontRight.setPower(BLFR/Math.abs(BRFL));
+                backLeft.setPower(-BLFR/Math.abs(BRFL));
+            }
 
+
+        }else{
+            frontLeft.setPower(-BRFL);
+            backRight.setPower(BRFL);
+            frontRight.setPower(BLFR);
+            backLeft.setPower(-BLFR);
+        }
     }
 }
